e8da0d2ea3b5c8a441ec1b746cadec3cecec20a3
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-06-28 21:15:10 +1000

    adding dark mode toggle

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const toogleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// dar or light img
+function imageMode(color) {
+	image1.src = `img/undraw_proud_coder_${color}.svg`;
+	image2.src = `img/undraw_feeling_proud_${color}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// dark mode styles
+function darkMode() {
+	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	toggleIcon.children[0].textContent = 'Dark Mode';
+	toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+	imageMode('dark');
+}
+
+// light mode styles
+function lightMode() {
+	nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	toggleIcon.children[0].textContent = 'Light Mode';
+	toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+	imageMode('dark');
+}
 
 // switch theme dynamically
 function switchTheme(event) {
 if (event.target.checked) {
 	document.documentElement.setAttribute('data-theme', 'dark');
+	darkMode();
 } else {
 	document.documentElement.setAttribute('data-theme', 'light');
+	lightMode();
 }
 }
 
