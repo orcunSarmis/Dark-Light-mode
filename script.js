8443e35d1b6434e4881574c9abfd567c1dfc448b
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-07-23 20:50:16 +1000

    code clean up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const toogleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
@@ -6,41 +6,64 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-// dar or light img
+// Dark or Light Images
 function imageMode(color) {
-	image1.src = `img/undraw_proud_coder_${color}.svg`;
-	image2.src = `img/undraw_feeling_proud_${color}.svg`;
-	image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// dark mode styles
-function darkMode() {
-	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-	toggleIcon.children[0].textContent = 'Dark Mode';
-	toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-	imageMode('dark');
-}
-
-// light mode styles
-function lightMode() {
-	nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-	textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-	toggleIcon.children[0].textContent = 'Light Mode';
+// code cleaning function
+function toggleDarkLightMode(isDark) {
+	nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+	textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+	toggleIcon.children[0].textContent =isDark ? 'Dark Mode' : 'Light Mode';
+	isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
 	toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-	imageMode('dark');
+	isDark ? imageMode('dark') : imageMode('light');
 }
 
-// switch theme dynamically
+// Dark Mode Styles
+// function darkMode() {
+//   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//   toggleIcon.children[0].textContent = 'Dark Mode';
+//   toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+//   imageMode('dark');
+// }
+
+// Light Mode Styles
+// function lightMode() {
+//   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//   toggleIcon.children[0].textContent = 'Light Mode';
+//   toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+//   imageMode('light');
+// }
+
+// Switch Theme Dynamically
 function switchTheme(event) {
-if (event.target.checked) {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	darkMode();
-} else {
-	document.documentElement.setAttribute('data-theme', 'light');
-	lightMode();
-}
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(true);
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    toggleDarkLightMode(false);
+  }
 }
 
-// eventListner
-toogleSwitch.addEventListener('change', switchTheme);
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
